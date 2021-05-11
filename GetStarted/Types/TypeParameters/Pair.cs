7b29d7a32e3cf54d766ef1b9e1de52ccbe08f617
Rafael Pioli <rspioli.rf@gmail.com>
2021-05-11 15:44:42 -0300

    GetStarted -> Types -> TypeParameters

---
+++ GetStarted/Types/TypeParameters/Pair.cs
@@ -0,0 +1,10 @@
+namespace TypeParameters
+{
+    public class Pair<TFirst, TSecond>
+    {
+        public TFirst First { get; }
+        public TSecond Second { get; }
+
+        public Pair(TFirst first, TSecond second) => (First, Second) = (first, second);
+    }
+}
---
+++ GetStarted/Types/TypeParameters/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TypeParameters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pair<string, string> p1 = new Pair<string, string>("id", "8hdh46ffs3738sloi4");
+
+            Console.WriteLine($"First: {p1.First}");
+            Console.WriteLine($"Second: {p1.Second}");
+        }
+    }
+}
