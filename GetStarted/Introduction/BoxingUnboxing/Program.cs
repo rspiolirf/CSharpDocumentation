f974c8ce72d6992346271d597086c4ca8ea98734
Rafael Pioli <rspioli.rf@gmail.com>
2021-05-11 14:50:37 -0300

    GetStarted -> Introduction -> BoxingUnboxing

---
+++ GetStarted/Introduction/BoxingUnboxing/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace BoxingUnboxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 20;
+            object o = i; // boxing
+            int j = (int)o; // unboxing
+
+            Console.WriteLine($"i: {i}");
+            Console.WriteLine($"o: {o}");
+            Console.WriteLine($"j: {j}");
+        }
+    }
+}
