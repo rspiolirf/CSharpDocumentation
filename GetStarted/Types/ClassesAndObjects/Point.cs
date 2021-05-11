0f51d7a001b4a1f8efdc82b582e993fba555bb1e
Rafael Pioli <rspioli.rf@gmail.com>
2021-05-11 15:23:58 -0300

    GetStarted -> Types -> ClassesAndObjects

---
+++ GetStarted/Types/ClassesAndObjects/Point.cs
@@ -0,0 +1,10 @@
+namespace ClassesAndObjects
+{
+    public class Point
+    {
+        public int X { get; }
+        public int Y { get; }
+
+        public Point(int x, int y) => (X, Y) = (x, y);
+    }
+}
---
+++ GetStarted/Types/ClassesAndObjects/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ClassesAndObjects
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Point p1 = new Point(0, 0);
+            Point p2 = new Point(10, 20);
+
+            Console.WriteLine($"Point p1: ({p1.X}, {p1.Y})");
+            Console.WriteLine($"Point p2: ({p2.X}, {p2.Y})");
+        }
+    }
+}
