e8ddf4f48993fa4fc332f74344eb1c2625883d4a
Rafael Pioli <rspioli.rf@gmail.com>
2021-05-11 22:53:42 -0300

    GetStarted -> Types -> Tuples

---
+++ GetStarted/Types/Tuples/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Tuples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (int I, string S) t1 = (4, "Mastering CSharp");
+            Console.WriteLine(t1.I);
+            Console.WriteLine(t1.S);
+        }
+    }
+}
