8b273df2d86b9180132dcc7c287cbc92ada3dbef
Rafael Pioli <rspioli.rf@gmail.com>
2021-05-11 22:48:55 -0300

    GetStarted -> Types -> NullableTypes

--- GetStarted/Types/NullableTypes/Program.cs
+++ GetStarted/Types/NullableTypes/Program.cs
@@ -2,11 +2,19 @@
 
 namespace NullableTypes
 {
+    #nullable enable
+    
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int? optionalInt = default;
+            optionalInt = 5;
+            Console.WriteLine(optionalInt);
+
+            string? optionalString = default;
+            optionalString = "Hello CSharp!";
+            Console.WriteLine(optionalString);
         }
     }
 }
